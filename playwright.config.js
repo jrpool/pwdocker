6123e70c2f409e2772085c4bc34301fa009eacc6
Jonathan Pool <pool@jpdev.pro>
2025-02-21 21:08:28 -0500

    experiment

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   // Reporter to use
-  reporter: 'json',
+  reporter: 'html',
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,10 +1,20 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const name0 = await page.innerText('.navbar__title');
-  expect(name0).toBe('Playwright');
-  await page.goto('https://example.com/');
-  const name1 = await page.innerText('h1');
-  expect(name1).toBe('Example Domain');
+test.describe('main suite', () => {
+
+  test('Playwright home page', async ({page}) => {
+    await page.goto('https://playwright.dev/');
+    const name0 = await page.innerText('.navbar__title');
+    expect(name0).toBe('Playwright');
+  });
+
+  test('Example', async ({page}) => {
+    await page.goto('https://example.com/');
+    const name1 = await page.innerText('h1');
+    expect(name1).toBe('Example Domain');
+    const docLoc = page.getByRole('document');
+    const docLang = (await docLoc.getAttribute('lang')) || '';
+    expect(docLang.length).toBeGreaterThan(0);
+  });
+
 });
